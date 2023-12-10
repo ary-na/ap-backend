e3df4c7efc4c2bf4eb8e26cf2a10b432e8a29e02
Arian N. Yamchelo <ariannajafi@icloud.com>
2023-12-10 12:43:51 +1100

    Optimise code.

--- Utils.js
+++ Utils.js
@@ -1,38 +1,41 @@
 // @file ./Utils.js
 
 // Setup dependencies for the Utils class.
-require("dotenv").config()
-const nodemailer = require('nodemailer')
-const emailValidator = require('email-validator')
+require("dotenv").config();
+const nodemailer = require('nodemailer');
+const emailValidator = require('email-validator');
 
 class Utils {
-    validateName(name) {
-        const excludedNames = ['Test', 'test', 'Admin', 'admin', 'User', 'user']
-        const nameRegexPattern = `^(?!${excludedNames.join('|')}$)[A-Za-z\\s'-]+$`
-        const nameRegex = new RegExp(nameRegexPattern)
-
-        return nameRegex.test(name)
-    }
-
-    validateEmail(email) {
-        return emailValidator.validate(email)
-    }
-
-    validateMessage(message) {
-        const messageRegexPattern = /^[a-zA-Z0-9,.!?()\s]+$/
-        const messageRegex = new RegExp(messageRegexPattern)
-
-        return messageRegex.test(message)
-    }
-    async sendEmail(to, subject, text) {
-        const transporter = await nodemailer.createTransport({
+    constructor() {
+        this.transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
                 user: process.env.EMAIL_ADDRESS,
                 pass: process.env.EMAIL_PASSWORD
             },
         });
+    }
 
+    validateName(name) {
+        const excludedNames = ['Test', 'test', 'Admin', 'admin', 'User', 'user'];
+        const nameRegexPattern = `^(?!${excludedNames.join('|')}$)[A-Za-z\\s'-]+$`;
+        const nameRegex = new RegExp(nameRegexPattern);
+
+        return nameRegex.test(name);
+    }
+
+    validateEmail(email) {
+        return emailValidator.validate(email);
+    }
+
+    validateMessage(message) {
+        const messageRegexPattern = /^[a-zA-Z0-9,.!?()\s]+$/;
+        const messageRegex = new RegExp(messageRegexPattern);
+
+        return messageRegex.test(message);
+    }
+
+    async sendEmail(to, subject, text) {
         const mailOptions = {
             from: process.env.EMAIL_ADDRESS,
             to,
@@ -40,9 +43,9 @@ class Utils {
             text,
         };
 
-        return await transporter.sendMail(mailOptions);
-    };
+        return await this.transporter.sendMail(mailOptions);
+    }
 }
 
 // Export the Utils class as a module.
-module.exports = new Utils()
+module.exports = new Utils();
--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 // @file    ./index.js
 
 // Dependencies ----------------------------------------------------------------
-const Utils = require("./Utils")
+const Utils = require("./Utils");
 
 // Setup routes ----------------------------------------------------------------
 
 // - Contact me route
 exports.handler = async (event, context) => {
     try {
-        const body = JSON.parse(event.body)
+        const body = JSON.parse(event.body);
 
         // Check if body is missing.
-        if (!body.name || !body.email || !body.message)
+        if (!body.name || !body.email || !body.message) {
             return {
                 statusCode: 400,
                 body: JSON.stringify({message: "Body is missing!"})
-            }
+            };
+        }
 
         // Validate body.
-        if (!Utils.validateName(body.name) || !Utils.validateEmail(body.email) || !Utils.validateMessage(body.message))
+        if (!Utils.validateName(body.name) || !Utils.validateEmail(body.email) || !Utils.validateMessage(body.message)) {
             return {
                 statusCode: 400,
                 body: JSON.stringify({message: "Invalid body provided!"})
-            }
+            };
+        }
 
-        const {name, email, message} = body
+        const {name, email, message} = body;
 
         // Send email
-        await Utils.sendEmail(email, `I got your message ${name}`, `Name: ${name}\nEmail: ${email}\nMessage: ${message}`)
+        await Utils.sendEmail(email, `I got your message ${name}`, `Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
 
         return {
             statusCode: 200,
@@ -37,6 +39,6 @@ exports.handler = async (event, context) => {
         return {
             statusCode: 500,
             body: JSON.stringify({message: 'Internal server error.'})
-        }
+        };
     }
-}
+};
